452cd9fe786c82b4f197436fead7849c4dc823f2
SanjayGrover <161492553+SanjayGrover@users.noreply.github.com>
2024-03-01 13:48:22 +0000

    import numpy as np
    class Perceptron():
      def __int__(self,inputs,bias=1.0):
        self.weights=(np.random.rand(inputs+1) * 2)-1
        self.bias=bias
      def run(self,x):
        sum=np.dot(np.append(x,self.bias),self.weights)
        return self.sigmoid(sum)
      def set_weights(self,*x_init):
        self.weights=np.array(x_init)
      def sigmoid(self,x):
        return 1/(1+np.exp(-x))
    
    neuron=Perceptron()
    neuron.set_weights(15,15,-10)
    
    print("OR Gate : ")
    print(f'0 0 : {neuron.run([0,0])}')
    print(f'0 1 : {neuron.run([0,1])}')
    print(f'1 0 : {neuron.run([1,0])}')
    print(f'1 1 : {neuron.run([1,1])}')

---
+++ src/perceptron_for_OR_Gate.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class Perceptron():
+  def __int__(self,inputs,bias=1.0):
+    self.weights=(np.random.rand(inputs+1) * 2)-1
+    self.bias=bias
+  def run(self,x):
+    sum=np.dot(np.append(x,self.bias),self.weights)
+    return self.sigmoid(sum)
+  def set_weights(self,*x_init):
+    self.weights=np.array(x_init)
+  def sigmoid(self,x):
+    return 1/(1+np.exp(-x))
+  
+neuron=Perceptron()
+neuron.set_weights(15,15,-10)
+
+print("OR Gate : ")
+print(f'0 0 : {neuron.run([0,0])}')
+print(f'0 1 : {neuron.run([0,1])}')
+print(f'1 0 : {neuron.run([1,0])}')
+print(f'1 1 : {neuron.run([1,1])}')
